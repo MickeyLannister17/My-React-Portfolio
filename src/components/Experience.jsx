3b4d6e3b0ccd87f6ddfbdc718f5c5a14e77156b7
Senaike Michael <senaikepooldis@gmail.com>
2023-01-20 14:23:43 +0000

    omega commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -77,7 +77,7 @@ const Experience = () => {
     <div
       name="experience"
       className="bg-gradient-to-b from-gray-800 to-black
-    w-full h-screen"
+    w-full"
     >
       <div
         className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center
