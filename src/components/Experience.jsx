876a0d1f9cbe4e76b4b1331042fcd43bbea7db88
Senaike Michael <senaikepooldis@gmail.com>
2023-01-20 18:49:35 +0000

    fourth

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -76,31 +76,21 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black
-    w-full h-screen py-16"
+      className="bg-gradient-to-b from-gray-800 to-black w-full "
     >
-      <div
-        className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center
-      w-full h-full text-white"
-      >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
-          <p
-            className="text-4xl font-bold border-b-4 border-gray-500
-          p-2 inline"
-          >
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 pt-16 md:py-0 inline">
             Experience
           </p>
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
-        <div
-          className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8
-        text-center py-8 px-12 sm:px-0"
-        >
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
-              className={`shadow-md hover:scale-105duration-500 py-2
-              rounded-lg ${style}`}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
               <img src={src} alt="" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,43 +1,44 @@
 import BankPage from "../assets/bankpage.jpg";
-
+import BankPagePc from "../assets/bankpagepc.jpg";
 import CinematicMob from "../assets/cinematicmob.jpg";
-
+import CinematicPc from "../assets/cinematic.jpg";
 import InventoryMob from "../assets/inventorymob.jpg";
-
+import InventoryPc from "../assets/inventorypc.jpg";
 import MyManageMob from "../assets/mymanagepc.jpg";
-
+import MyManagePc from "../assets/mymanage.jpg";
 import NubiaFoodMob from "../assets/nubiafood.jpg";
+import NubiaFoodPc from "../assets/nubiafoodpc.jpg";
 
 const Portfolio = () => {
   const portfolioPics = [
     {
       id: 1,
-      src: BankPage,
+      src: BankPagePc,
       demo_link: "https://mybankpage.netlify.app",
       code_link: "https://github.com/MickeyLannister17/hoobank-landing-page",
     },
     {
       id: 2,
-      src: CinematicMob,
+      src: CinematicPc,
       demo_link: "https://nubiacinematic.netlify.app/",
       code_link: "https://github.com/MickeyLannister17/cinematic",
     },
     {
       id: 3,
-      src: InventoryMob,
+      src: InventoryPc,
       demo_link: "https://myinventoryapp.netlify.app/",
       code_link: "https://github.com/MickeyLannister17/My-React-Inventory-App",
     },
     {
       id: 4,
-      src: MyManageMob,
+      src: MyManagePc,
       demo_link: "https://mymanage.netlify.app/",
       code_link:
         "https://github.com/MickeyLannister17/landing-page-tailwind-project",
     },
     {
       id: 5,
-      src: NubiaFoodMob,
+      src: NubiaFoodPc,
       demo_link: "https://mickeylannister17.github.io/Nubia-Food/",
       code_link: "https://github.com/MickeyLannister17/Nubia-Food",
     },
@@ -45,38 +46,23 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black
-    to-gray-800 text-white md:h-full py-16"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
-      <div
-        className="max-w-screen-lg p-4 mx-auto
-    flex flex-col justify-center w-full h-full md:mb-5"
-      >
-        <div className="pb-8 mt-5 md:mt-0">
-          <p
-            className="text-4xl font-bold inline
-          border-b-4 border-gray-500"
-          >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className="py-6 ">Check out some of my work right here</p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
-        {}
-        <div
-          className="grid sm:grid-cols-3 md:grid-col-3
-        gap-8 px-12 sm:px-0"
-        >
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {portfolioPics.map(({ id, src, demo_link, code_link }) => (
-            <div
-              key={id}
-              className="shadow-md shadow-gray-600 hover:scale-105 duration-300
-          rounded-lg"
-            >
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
-                className="rounded-md 
-            duration-200"
+                className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
                 <a
