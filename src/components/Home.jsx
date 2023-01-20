8b1c5aa2c0353463de647da60a57503f5a63fb49
Senaike Michael <senaikepooldis@gmail.com>
2023-01-20 19:09:41 +0000

    seventh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,12 +7,12 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b 
-     from-black via-black to-gray-800 py-10"
+      className="h-full md:h-screen w-full bg-gradient-to-b 
+     from-black via-black to-gray-800 py-10 md:py-0"
     >
       <div
         className="max-w-screen-lg mx-auto flex flex-col items-center
-      justify-center h-screen px-4 md:flex-row "
+      justify-center h-screen px-4 md:flex-row py-10 md:py-0"
       >
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
@@ -23,28 +23,30 @@ const Home = () => {
             HNGi8 Finalist|Frontend & Backend Engineer
             React.js|Redux|FastAPI|Django|REST API|Mongo DB|Tailwind
           </p>
-          <div className="">
-            <Link
-              to="portfolio"
-              smooth
-              duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
+          <div className="flex gap-3">
+            <div>
+              <Link
+                to="portfolio"
+                smooth
+                duration={500}
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
             bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Portfolio
-              <span className="group-hover:rotate-90 duration-300">
-                <AiOutlineArrowRight size={25} className="ml-1" />
-              </span>
-            </Link>
-          </div>
-          <div>
-            {/* <a
-              href=""
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
+              >
+                Portfolio
+                <span className="group-hover:rotate-90 duration-300">
+                  <AiOutlineArrowRight size={25} className="ml-1" />
+                </span>
+              </Link>
+            </div>
+            <div>
+              <a
+                href=""
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
             bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Resume
-            </a> */}
+              >
+                Resume
+              </a>
+            </div>
           </div>
         </div>
         <div>
@@ -52,7 +54,7 @@ const Home = () => {
             src={LannisterImage}
             alt="my profile"
             className="rounded-2xl
-          mx-auto h-[400px] w-[400px] md:w-[600px]"
+          mx-auto h-[350px] w-[350px] md:w-[600px]"
           />
         </div>
       </div>
