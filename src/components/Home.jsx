584624ff2f7f4aa72a2916d93445a87b9bce0218
Senaike Michael <senaikepooldis@gmail.com>
2023-01-20 14:32:08 +0000

    romeo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div
       name="home"
       className="h-screen w-full bg-gradient-to-b 
-     from-black via-black to-gray-800 mt-4"
+     from-black via-black to-gray-800"
     >
       <div
         className="max-w-screen-lg mx-auto flex flex-col items-center
@@ -23,28 +23,30 @@ const Home = () => {
             HNGi8 Finalist|Frontend & Backend Engineer
             React.js|Redux|FastAPI|Django|REST API|Mongo DB|Tailwind
           </p>
-          <div>
-            <Link
-              to="portfolio"
-              smooth
-              duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
+          <div className="flex">
+            <div>
+              <Link
+                to="portfolio"
+                smooth
+                duration={500}
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
+            bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer mr-4"
+              >
+                Portfolio
+                <span className="group-hover:rotate-90 duration-300">
+                  <AiOutlineArrowRight size={25} className="ml-1" />
+                </span>
+              </Link>
+            </div>
+            <div>
+              <a
+                href=""
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
             bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Portfolio
-              <span className="group-hover:rotate-90 duration-300">
-                <AiOutlineArrowRight size={25} className="ml-1" />
-              </span>
-            </Link>
-          </div>
-          <div>
-            <a
-              href=""
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
-            bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              Resume
-            </a>
+              >
+                Resume
+              </a>
+            </div>
           </div>
         </div>
         <div>
